d3909e7f7b87cd6153314b8bc9d336f71109187a
bond0010732 <122272180+bond0010732@users.noreply.github.com>
2025-08-22 08:02:31 +0100

    Update BetModel.js

--- models/BetModel.js
+++ models/BetModel.js
@@ -1,21 +1,11 @@
 const mongoose = require('mongoose');
 
 const BetSchema = new mongoose.Schema({
-    roomId: {
-        type: String,
-    },
-    playerName: {
-        type: String,
-        required: true,
-    },
-    betAmount: {
-        type: Number,
-        required: true,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+userId: { type: mongoose.Schema.Types.ObjectId, ref: "OdinCircledbModel", required: true },
+  roomId: { type: String, required: true },
+  amount: { type: Number, required: true },
+  type: { type: String, enum: ["placed", "won", "lost"], default: "placed" },
+  createdAt: { type: Date, default: Date.now }
 });
 
 const BetModel = mongoose.model('Bet', BetSchema);
