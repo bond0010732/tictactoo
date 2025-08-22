ffd63e080c6b58566320219aa7e0b04fbc6b9068
bond0010732 <122272180+bond0010732@users.noreply.github.com>
2025-08-22 11:35:13 +0100

    Update BetModel.js

--- models/BetModel.js
+++ models/BetModel.js
@@ -4,7 +4,7 @@ const BetSchema = new mongoose.Schema({
 userId: { type: mongoose.Schema.Types.ObjectId, ref: "OdinCircledbModel", required: true },
   roomId: { type: String, required: true },
   amount: { type: Number, required: true },
-  type: { type: String, enum: ["bet", "win", "lost"], default: "placed" },
+  type: { type: String, enum: ["bet", "win", "lost"], default: "bet" },
   createdAt: { type: Date, default: Date.now }
 });
 
